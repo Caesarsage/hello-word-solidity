af832a87fd124dfc76953add536e2847a0038da0
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-03-09 14:11:35 +0100

    hello world solidity

---
+++ helloworld.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+  string public myString;
+
+  constructor() {
+    myString = "Hello world";
+  }
+
+  function setString(string memory _myString) public{
+    myString = _myString;
+  }
+
+  function getString() public view returns(string memory){
+      return myString;
+  }
+
+   fallback() external{}
+   
+}
